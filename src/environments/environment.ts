b1babc2ea2c9f4d2232955b521024ed8e191ded8
rocklessg <abdulhafiz.suleiman@decagon.dev>
2024-06-18 15:02:34 +0100

    organic-shop initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyBa9qk3OEZKRsjzmR8IxcRSYfH-EvE53bQ",
+        authDomain: "organic-shop-e185c.firebaseapp.com",
+        projectId: "organic-shop-e185c",
+        storageBucket: "organic-shop-e185c.appspot.com",
+        messagingSenderId: "137383402011",
+        appId: "1:137383402011:web:0c570fb49d7d882c3876be"
+    }
+};
