912ffbac298d5c3ca3c733fe53b036e4f96742c6
rocklessg <abdulhafiz.suleiman@decagon.dev>
2024-06-18 18:29:05 +0100

    Complete navbar implementations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,36 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 
 const routeConfig: Routes = [
     {
     path: '',
     component: HomeComponent,
     title: 'Home page',
-  },
-  {
-    path: 'shopping-cart',
-    component: ShoppingCartComponent,
-    title: 'Cart',
-  },
+    },
+    {
+        path: 'shopping-cart',
+        component: ShoppingCartComponent,
+        title: 'Cart',
+    },
+    {
+        path: 'my/orders',
+        component: MyOrdersComponent,
+        title: 'My Orders',
+    },
+    {
+        path: 'admin/orders',
+        component: AdminOrdersComponent,
+        title: 'Manage Orders',
+    },
+    {
+        path: 'admin/products',
+        component: AdminProductsComponent,
+        title: 'My Orders',
+    }
 ];
 
 export default routeConfig;
