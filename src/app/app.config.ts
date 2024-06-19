89aad3b7771211d0a24feb311e3143511d6bf50f
rocklessg <abdulhafiz.suleiman@decagon.dev>
2024-06-19 23:23:05 +0100

    Implement google authentication

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,22 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import  routeConfig from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideHttpClient } from '@angular/common/http';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import routeConfig from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routeConfig), provideClientHydration(), provideFirebaseApp(() => initializeApp({"projectId":"organic-shop-e185c","appId":"1:137383402011:web:0c570fb49d7d882c3876be","storageBucket":"organic-shop-e185c.appspot.com","apiKey":"AIzaSyBa9qk3OEZKRsjzmR8IxcRSYfH-EvE53bQ","authDomain":"organic-shop-e185c.firebaseapp.com","messagingSenderId":"137383402011"})), provideFirestore(() => getFirestore())]
+  providers: [
+    provideRouter(routeConfig),
+    provideHttpClient(),
+    provideFirebaseApp(() => initializeApp({
+      projectId: "organic-shop-e185c",
+      appId: "1:137383402011:web:0c570fb49d7d882c3876be",
+      storageBucket: "organic-shop-e185c.appspot.com",
+      apiKey: "AIzaSyBa9qk3OEZKRsjzmR8IxcRSYfH-EvE53bQ",
+      authDomain: "organic-shop-e185c.firebaseapp.com",
+      messagingSenderId: "137383402011"
+    })),
+    provideAuth(() => getAuth())
+  ],
 };
