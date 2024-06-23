d6c2ef36adb01ded7afd4ffe7219d4ffc01e1738
rocklessg <abdulhafiz.suleiman@decagon.dev>
2024-06-23 20:42:51 +0100

    Implement logged in user name display

--- src/app/bootstrap-navbar/bootstrap-navbar.component.ts
+++ src/app/bootstrap-navbar/bootstrap-navbar.component.ts
@@ -1,17 +1,31 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
-import { Auth } from '@angular/fire/auth';
+import { Auth, onAuthStateChanged, User } from '@angular/fire/auth';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'bootstrap-navbar',
   standalone: true,
-  imports: [NgbDropdownModule],
+  imports: [NgbDropdownModule, CommonModule],
   templateUrl: './bootstrap-navbar.component.html',
   styleUrl: './bootstrap-navbar.component.css'
 })
 class BootstrapNavbarComponent implements OnInit {
   private auth: Auth = inject(Auth);
-  constructor() { }
+ // user = this.auth.currentUser;   
+  user: User | null = null; 
+
+  constructor() { 
+    // Listen for authentication state changes
+    onAuthStateChanged(this.auth, (user) => {
+      this.user = user;
+    });
+  }
+
+  // logUser(event: Event) {
+  //   event.preventDefault(); // Prevent the default action
+  //   console.log(`UserObj is the one: ${this.user?.displayName} None`);
+  // } 
   
   logout() {
     this.auth.signOut();
