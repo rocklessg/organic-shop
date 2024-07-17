364d250257ec77295bf82e716c40c8f8d1ec60e4
rocklessg <abdulhafiz.suleiman@decagon.dev>
2024-07-17 12:05:37 +0100

    Update: removed commented codes

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -24,29 +24,3 @@ export class AuthGuardService implements CanActivate {
     );
   }
 }
-
-
-
-
-// import { Injectable } from '@angular/core';
-// import { CanActivate, Router } from '@angular/router';
-// import { AuthService } from './auth.service';
-// import 'rxjs/add/operator/map';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class AuthGuardService implements CanActivate {
-
-//   constructor(private auth: AuthService, private router: Router) { }
-
-//   canActivate() {
-//     return this.auth.user$.map(user => {
-//       if (user)
-//         return;
-
-//       this.router.navigate(['/login'])
-//       return false
-//     }); 
-//   }
-// }
